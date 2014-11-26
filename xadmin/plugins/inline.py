609f061c87948115a22a86afcbbb91866eeb7544
Roger Palacios <rpalacios@mycodeworkshop.com>
2014-11-26 12:32:28 -0500

    [FIX] ShowField template replacing

--- xadmin/plugins/inline.py
+++ xadmin/plugins/inline.py
@@ -17,7 +17,7 @@ class ShowField(Field):
     def __init__(self, admin_view, *args, **kwargs):
         super(ShowField, self).__init__(*args, **kwargs)
         self.admin_view = admin_view
-        if admin_view.style == 'table':
+        if issubclass(_class, TableInlineStyle):
             self.template = "xadmin/layout/field_value_td.html"
 
     def render(self, form, form_style, context):
